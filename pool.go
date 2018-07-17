b3aad90fd93e4e4af10b50533acb6a98bc66aeb3
Unknown <iliamail05@gmail.com>
2018-07-17 19:50:11 +0300

    temp

--- pool.go
+++ pool.go
@@ -53,6 +53,25 @@ func (p *Pool) LoadParams(params string) (err error) {
 func poolItemCreate(v mfe.Variant) (pi PoolItem, err error) {
 	pi = PoolItem{}
 
+	pi.DriverName = v.GE("driver_name").Str()
+	pi.DataSourceName = v.GE("data_source_name").Str()
+
+	db, err := sql.Open(pi.DriverName, pi.DataSourceName)
+	if err != nil {
+		return pi, nil
+	}
+
+	pi.DB = db
+
+	ps := v.GE("pool_size")
+	mps := v.GE("min_pool_size")
+
+	if ps.IsDecimal() {
+		pi.DB.SetMaxOpenConns(ps.Dec())
+	}
+
+	pi.DB
+
 	return pi, nil
 }
 
